668e7e7d67c725e334e04c4ee8bea27862cfb8a4
SabrinaBarros <sabrina.barros2045@gmail.com>
2021-04-17 23:30:59 -0300

    primeiro commit, primeira versão do projeto

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hiwari :smiley_cat:
+
+1 - Clone this repository:
+
+```sh
+$ git clone https://github.com/SabrinaBarros/hiwari.git
+```
+
+```sh
+$ cd hiwari
+```
+
+2 - Install all dependencies:
+
+```sh
+$ npm install
+```
---
+++ hiwari.js
@@ -0,0 +1,22 @@
+const json = require('./routine.json');
+const emoji = require('node-emoji');
+
+const log = json.studies_log;
+
+let allPoints = 0;
+let totalItens = 0;
+
+log.forEach( item => {
+
+    allPoints += item.complete_points;
+
+    if(item.complete_points !== null) {
+        totalItens += 1
+    }
+
+})
+
+const weeklyAvarege = allPoints / totalItens;
+const response = 'A media de pontos realizados por semana é de: ' + weeklyAvarege + ' pontos.' + emoji.get('fire');
+
+console.log(response);
